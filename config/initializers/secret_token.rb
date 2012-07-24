2e6ad5bff8af2ffa61880dea936c7b3246952bf6
Mathew Johnson <mathewgjohnson@gmail.com>
2012-07-24 10:33:02 -0700

    text change on home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog2::Application.config.secret_token = '6ad4345ac8ffd577b21b033a03bd0d7fc6cb8254621d61be66c85120d1a62553f68221dbbf031cb5d3ad9e1a655ce2c7d27529817e6cd8db395b5e55a86fa112'
