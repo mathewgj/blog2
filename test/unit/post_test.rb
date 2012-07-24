370ec4213b85fc37667a62420101381b71212105
Mathew Johnson <mathewgjohnson@gmail.com>
2012-07-24 11:50:51 -0700

    add another test

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -4,4 +4,8 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save post without title" do
+  post = Post.new
+  assert !post.save
+end
 end
